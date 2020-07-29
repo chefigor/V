785d1d0daf08d9ee98ec01463d8f97c4bbd712aa
Igor Jovanovic <igorjovanovic2898@gmail.com>
2020-07-29 22:35:49 +0200

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+	    private ApplicationDbContext _context;
+
+	    public MoviesController()
+	    {
+		    _context = new ApplicationDbContext();
+	    }
+
+	    protected override void Dispose(bool disposing)
+	    {
+			_context.Dispose();
+	    }
+
+	    public ActionResult Index()
+		{
+			var movies = _context.Movies.ToList();
+			return View(movies);
+		}
+
+    }
+}
